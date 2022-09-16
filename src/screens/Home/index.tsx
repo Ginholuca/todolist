9d8e927dd74e693f3723e0c19864a67d96720b46
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-09-16 08:50:44 -0300

    test

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { styles } from './styles'
 
 // import { Container } from './styles';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Salve</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Salve</Text>
     </View>
   )
 }
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  text: {
+    fontSize: 60,
+    color: '#000',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
