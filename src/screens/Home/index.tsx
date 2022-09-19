1baa878d6f96ccfbde99299d4f2724520dfbe5bd
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-09-19 09:38:21 -0300

    add somethings

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { styles } from './styles'
 
-// import { Container } from './styles';
+import logo from '../../assets/Logo.png'
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Salve</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Image source={logo} stule={styles.logo} />
+        <View style={styles.top}></View>
+      </View>
+    </>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,22 @@
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1A1A1A',
+  },
   text: {
     fontSize: 60,
     color: '#000',
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  logo: {
+    width: '100%',
+    height: 70,
+  },
+  top: {
+    width: 375,
+    height: 173,
+    backgroundColor: '#0D0D0D',
+    top: 0,
   },
 })
