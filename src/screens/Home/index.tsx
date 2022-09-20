7d1d0b2fa5a550456260ba765526284ad38cfc9c
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-09-19 21:44:13 -0300

    styling

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,52 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import { styles } from './styles'
 
-import logoImg from '../../assets/Logo.png'
+import LogoSvg from '../../assets/Logo.svg'
+import PlusSvg from '@assets/plus.svg'
 
-export default function Home() {
+export function Home() {
   return (
-    <>
-      <View style={styles.container}>
-        <Image source={logoImg} stule={styles.logo} />
-        <View style={styles.top}></View>
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <LogoSvg
+          style={{
+            position: 'absolute',
+            top: 60,
+            left: 110,
+          }}
+          width={150}
+          height={42}
+        />
       </View>
-    </>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor="#6B6B6B"
+          // onChangeText={setParticipantName}
+          // value={participantName}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          // onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}>
+            <PlusSvg height={62} width={22} />
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.below}>
+        <Text style={styles.created}>Criadas</Text>
+        <View style={styles.zeroborder}>
+          <Text style={styles.zero}>0</Text>
+        </View>
+        <Text style={styles.completed}>Concluídas</Text>
+        <View style={styles.zeroborder}>
+          <Text style={styles.zero}>0</Text>
+        </View>
+      </View>
+    </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -9,14 +9,88 @@ export const styles = StyleSheet.create({
     fontSize: 60,
     color: '#000',
   },
-  logo: {
-    width: 250,
-    height: 70,
-  },
   top: {
     width: 375,
     height: 173,
     backgroundColor: '#0D0D0D',
     top: 0,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: '#262626',
+    borderRadius: 5,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16,
+    marginRight: 6,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
+
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#1E6F9F',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  plus: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  form: {
+    paddingHorizontal: 18,
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: -30,
+  },
+
+  below: {
+    paddingTop: 36,
+    paddingBottom: 18,
+    flexDirection: 'row',
+    borderBottomWidth: 2,
+    marginHorizontal: 18,
+    borderColor: '#262626',
+  },
+
+  created: {
+    fontSize: 16,
+    color: '#4EA8DE',
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+
+  completed: {
+    fontSize: 16,
+    color: '#8284FA',
+    fontWeight: 'bold',
+    paddingLeft: 125,
+    paddingRight: 10,
+  },
+
+  zero: {
+    paddingHorizontal: 3,
+    fontSize: 14,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  zeroborder: {
+    marginTop: 1,
+    backgroundColor: '#262626',
+    borderColor: '#262626',
+    borderTopWidth: 2,
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderWidth: 3,
+    borderRadius: 10,
   },
 })
